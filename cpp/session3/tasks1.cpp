9686aa14b79f50c5b19a8d520262c00bb3581a5d
AbdelRahman Yassin <abdelrahman.ahmed.yassin03@gmail.com>
2024-07-29 20:40:55 +0300

    session3 OOP Tasks slide 1

---
+++ cpp/session3/tasks1.cpp
@@ -0,0 +1,44 @@
+/* File Name : tasks1.cpp */
+/* The Author is : AbdelRahman Ahmed Yassin */
+/* The Date Of The File : 2024-07-28 */
+#include <cctype>
+#include<iostream>
+#include <csignal> 
+#include <algorithm>
+#include <climits>
+#include <iostream>
+#include <vector>
+bool is_even(int x){
+    return !(x % 2);
+}
+
+
+int main(){
+    
+    //task_1
+    char ch = '3';
+    if(std::isdigit(ch)){
+        std::cout << "Yes is digit" << std::endl;
+    }
+    else{
+        std::cout << "No" << std::endl;
+    } 
+    
+    //task_2
+     std::vector<int> vec {5 , 10 , 3 , 4 , 15 , 40 , 43};
+
+    
+    for (auto i : vec){
+        if(!(is_even(i))){
+            std::cout << "The digit : " << i << " is not even" << std::endl;
+            break;
+        }
+    } 
+    //task_3 
+    for (auto i : vec){
+        if((is_even(i))){
+            std::cout << "The digit : " << i << " is even" << std::endl;
+        }
+    }  
+
+}
